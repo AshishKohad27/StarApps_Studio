4b3aa1655bab04313836a2c93d8beccf6e66b131
AshishKohad27 <kohadashish27@gmail.com>
2023-04-13 17:58:41 +0530

    Done with docStrings

--- script/index.js
+++ script/index.js
@@ -1,37 +1,63 @@
-
+// Importing json file
 import UmbrellaArr from "../../Umbrella.json" assert {type: "json"}
 
+// Getting all id or class 
 let main = document.getElementById("main");
 let setImg = document.getElementById("setImg");
 let upload = document.getElementById("upload");
 let loaderSvg = document.getElementById("loaderSvg");
+
+// Id of uploading button and its loader
 let logo_label = document.getElementById("logo_label");
 let loader_UploadLogo = document.getElementById("loader_UploadLogo");
+
+// umbrella image displayed 
 let umbrellaDiv = document.getElementById("umbrellaDiv");
+
 //color Palette button
 document.getElementById("pink").addEventListener("click", () => colorPalette("pink"));
 document.getElementById("blue").addEventListener("click", () => colorPalette("blue"));
 document.getElementById("yellow").addEventListener("click", () => colorPalette("yellow"));
 
 
+// call back function whenever any color button selected will be invoked
 async function colorPalette(colorFromBtn) {
+
+    // we remove already present umbrella in our ui
     setImg.src = "";
+
+    // here data getting filter on the bases of parameters (i.e. colorFromBtn)
     let filter = UmbrellaArr.filter((item) => item.color === colorFromBtn)[0];
 
+    // after getting filter we set new background color , new upload button color and new umbrella base on selected umbrella 
     loaderSvg.style.fill = `${filter.colorBtn}`
     main.style.backgroundColor = `${filter.background}`;
     upload.style.backgroundColor = `${filter.colorBtn}`;
 
+    // some time image take to much time to load so set Promise and put condition
     await new Promise((resolve) => {
+        // when image load our promise will be resolved
         setImg.onload = resolve;
+
+        // here our image is loading on particular div
         setImg.src = `${filter.umbrella}`;
+
+        // but i don't want to display to user so i make it none 
         setImg.style.display = "none";
-        umbrellaDiv.style.display = "none";
+
+        // instead of displaying umbrella image while it is loading I am using loader
         loaderSvg.style.display = "block";
+
+        // logo should not be visible when umbrella image loading
+        umbrellaDiv.style.display = "none";
+
+        // same for upload button when new umbrella image is loaded upload svg change to loader div
         logo_label.style.display = "none";
         loader_UploadLogo.style.display = "block";
     });
 
+    //purpose of adding settime was to show loading functionality because some time image loading 
+    // happened with in a millisecond and loading indicator will not be displayed that why i added this 
     setTimeout(() => {
         setImg.style.display = "block";
         loaderSvg.style.display = "none"
--- script/upload.js
+++ script/upload.js
@@ -1,27 +1,35 @@
-const logo_input = document.querySelector("#logo_input");
-let displayLogo_name = document.getElementById("displayLogo_name");
-let deleteBtn = document.getElementById("deleteBtn");
-var Uploaded_logo = "";
-logo_input.addEventListener("change", function (e) {
+const logo_input = document.querySelector("#logo_input"); // input type="file"
+let displayLogo_name = document.getElementById("displayLogo_name"); // display : bottom of umbrella
+let deleteBtn = document.getElementById("deleteBtn"); // only visible when logo will upload
+var Uploaded_logo = ""; // it stored url of logo amd make it available globally 
 
-    console.log("i am get click")
-    //console.log(logo_input.value);//fake path
+logo_input.addEventListener("change", function (e) {
+    // Setting file name inside our upload button;
     displayLogo_name.innerHTML = e.target.files[0].name;
+
+    // Delete button will be visible once data has been uploaded
     deleteBtn.innerHTML = `<h1 id="delete_logo"><ion-icon name="close-outline"></ion-icon></h1>`;
     deleteBtn.addEventListener("click", () => {
         callDelete();
     })
+
+    // we are creating new file since we are getting fake path
     let reader = new FileReader();
     reader.addEventListener("load", () => {
+        //after load we get url of our logo
         Uploaded_logo = reader.result;
+
         document.querySelector("#display_logo>img").style.display = "block"
+        // then we set logo to desired div
         document.querySelector("#display_logo>img").src = `${Uploaded_logo}`;
-        console.log('Uploaded_logo:', Uploaded_logo)
+
+        // console.log('Uploaded_logo:', Uploaded_logo)
     });
 
     reader.readAsDataURL(this.files[0])
 })
 
+// it will remove all data which we set above
 function callDelete() {
     Uploaded_logo = ""
     displayLogo_name.innerHTML = "UPLOAD LOGO"
